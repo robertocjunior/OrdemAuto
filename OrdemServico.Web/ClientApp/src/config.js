1d5b96ad41abbc6880e2d8fdf384f28ed514891d
Roberto C Júnior <86366499+robertocjunior@users.noreply.github.com>
2025-11-12 12:04:11 -0300

    correção de erros do front end

--- OrdemServico.Web/ClientApp/src/config.js
+++ OrdemServico.Web/ClientApp/src/config.js
@@ -3,7 +3,7 @@ const env = process.env.NODE_ENV || 'development';
 
 const config = {
     development: {
-        baseApiUrl: 'http://localhost:5084/api' 
+        baseApiUrl: 'http://api:8080/api' 
     },
     production: {
         baseApiUrl: `${window.location.origin}/api`
